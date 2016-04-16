784994cafbaa966b17cf018ea4eede6ab5f9bae0
Aaron Sun <axiverse@gmail.com>
2016-04-16 13:26:15 -0700

    Initial code commit.

---
+++ CLI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Market;
+using Market.Exchange;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleOrderBook book = new SimpleOrderBook();
+
+            SimpleOrder a = new SimpleOrder(true, 11, 2);
+            SimpleOrder b = new SimpleOrder(false, 10, 1);
+
+            book.Add(b);
+            book.Add(a);
+
+            SimpleOrder c = new SimpleOrder(false, 10, 1);
+            book.Add(c);
+
+            Console.ReadKey();
+        }
+    }
+}
